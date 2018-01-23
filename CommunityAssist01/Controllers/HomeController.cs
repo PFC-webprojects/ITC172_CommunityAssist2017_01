d78181b8a48ec17f70935c46f6a8385372fa4ddc
PFC <PFC@PFC-DESKTOP2017>
2018-01-22 20:55:39 -0800

    Assignment 03 - Grant Types

--- CommunityAssist01/Controllers/HomeController.cs
+++ CommunityAssist01/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CommunityAssist01.Models;
 
 namespace CommunityAssist01.Controllers
 {
@@ -10,7 +11,8 @@ namespace CommunityAssist01.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            return View(db.GrantTypes.ToList());
         }
 
         public ActionResult About()
