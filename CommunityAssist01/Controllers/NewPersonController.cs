e9c6a0503bf0b78c63bf3785b27fa5f96c90dc25
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-02-09 00:29:46 -0800

    Assignment 04 - Registration

---
+++ CommunityAssist01/Controllers/NewPersonController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssist01.Models;
+
+namespace CommunityAssist01.Controllers
+{
+    public class NewPersonController : Controller
+    {
+        CommunityAssist2017Entities ca = new CommunityAssist2017Entities();
+
+        // GET: NewPerson
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include =
+            "LastName, " +
+            "FirstName, " +
+            "Apartment, " +
+            "Street, " +
+            "City, " +
+            "State, " +
+            "ZIPCode, " +
+            "Phone, " +
+            "EMail, " +
+            "PlainPassword"
+            )] NewPerson np)
+        {
+            int result = ca.usp_Register(
+                np.LastName,
+                np.FirstName,
+                np.EMail,
+                np.PlainPassword,
+                np.Apartment,
+                np.Street,
+                np.City,
+                np.State,
+                np.ZIPCode,
+                np.Phone
+                );
+
+            Message msg = new Message();
+            msg.Succeeded  =  result != -1;  //  The stored procedure usp_Register has been set up to return -1 if it fails.
+
+            return View("Result", msg);
+        }
+    }
+}
---
+++ CommunityAssist01/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist01.Models
+{
+    /*  As a slight change to the letter but not the spirit of the assignment instructions,
+     *  I added a bool property, Succeeded, to the Message class so that I could use that
+     *  to either include or exclude an extra ActionLink / anchor tag to Result.cshtml .
+     *  While I was at it, I refactored the MessageText property to a read-only property
+     *  whose value is determined by the value of Succeeded.
+     */ 
+    public class Message
+    {
+        public bool Succeeded { get; set; }
+        public string MessageText
+        {
+            get
+            {
+                return Succeeded ?
+                    "Thank you for registering." :
+                    "Sorry, but something seems to have gone wrong with the registration.";
+            }
+        }
+    }
+}
